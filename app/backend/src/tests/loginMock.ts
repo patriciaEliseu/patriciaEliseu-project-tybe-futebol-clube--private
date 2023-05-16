367ec4df6403d4e9ece73b4b20d9642237028504
Patricia Eliseu <patyeliseu4@gmail.com>
2023-05-16 15:48:29 -0300

    add testes do Teams

---
+++ app/backend/src/tests/loginMock.ts
@@ -0,0 +1,8 @@
+const loginMock =
+{
+   'token': `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+  .eyJkYXRhIjp7ImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInJvbGUiOiJ1c2VyIn0sImlhdCI6MTY4NDI2MDk1Nn0
+  .eeqIkvOUSIWBjNwT5W4pQXXChaYSAyjNkpsI1Tfi4qY`
+}
+
+export {loginMock};
---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,55 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Team from '../database/models/TeamModel';
+import { teamsMock } from './teamsMock';
+// import { Response } from 'superagent';
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+
+describe('Teste de integração usando getAll em /teams', function()  {
+  describe('list all teams and status 200', function() {
+    afterEach(() => {
+      sinon.restore();
+    });
+  it('Should return all teams', async function() {
+   
+   const response = await chai
+    .request(app)
+    .get('/teams');
+   expect(response.status).to.be.equal(200);
+   expect(response.body).to.deep.equal(teamsMock);
+  });
+});
+});
+
+describe('Teste de integração usando getById em /teams/:id', function()  {
+  describe('list all teams and status 200', function() {
+    afterEach(() => {
+      sinon.restore();
+    });
+  it('Should return team id: 2', async function() {
+   
+   const response = await chai
+    .request(app)
+    .get('/teams/2');
+   expect(response.status).to.be.equal(200);
+   expect(response.body).to.deep.equal(teamsMock[1]);
+  });
+  // it('Should not return team id:120, because does not exist', async function() {
+   
+  //   const response = await chai
+  //    .request(app)
+  //    .get('/teams/120');
+  //   expect(response.status).to.be.equal(500);
+  //   expect(response.body).to.deep.equal({ message: 'Deu errado' });
+  //  });
+});
+});
